559ab50f3038bef64610aa9ee2b65673eba6b32a
lxodud <xodud1998223@naver.com>
2024-04-29 20:50:21 +0900

    test: GroupFeature Demo 앱 구현

---
+++ aljo/Projects/Feature/GroupFeature/Demo/Sources/GroupDemoRootViewController.swift
@@ -0,0 +1,40 @@
+//
+//  GroupDemoRootViewController.swift
+//  GroupFeatureDemo
+//
+//  Created by 이태영 on 4/29/24.
+//  Copyright © 2024 com.asap. All rights reserved.
+//
+
+import UIKit
+
+import GroupFeatureImplementation
+
+final class GroupDemoRootViewController: UIViewController {
+  private let pushButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("push", for: .normal)
+    button.setTitleColor(.black, for: .normal)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+  }()
+  
+  override func viewDidLoad() {
+    view.addSubview(pushButton)
+    
+    NSLayoutConstraint.activate([
+      pushButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      pushButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
+    
+    let action = UIAction { [weak self] _ in
+      let viewController = GroupPrivacySelectionViewController()
+      self?.navigationController?.pushViewController(
+        viewController,
+        animated: true
+      )
+    }
+    
+    pushButton.addAction(action, for: .touchUpInside)
+  }
+}
